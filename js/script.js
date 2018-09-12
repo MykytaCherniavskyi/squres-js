14884a8eaf48df48b2e9301e530cc142d01baa92
mikitty_ch <mikita1234qwer@gmail.com>
2018-09-12 23:29:36 +0300

    Доделать появление удаления столбцов

--- js/script.js
+++ js/script.js
@@ -12,41 +12,75 @@ window.onload = (e) => {
     // squeres array
     let rows = document.getElementsByClassName('row');
 
+    //visibleMinus
+
+
     //addRows
     plusBottom.addEventListener('click', (e) => {
         let row = rows[0].cloneNode(true);
         center.appendChild(row);
+        if (rows.length === 2) {
+            minusLeft.style.visibility = 'visible';
+        }
     });
     //addColumn
     plusRight.addEventListener('click', (e) => {
         for (let i = 0; i < rows.length; i++) {
-            let innerRow = rows[i];
+            let row = rows[i];
             let singleSquere = document.querySelector('.squere-center').cloneNode(true);
             let spacing = center.childNodes[0].cloneNode(true);
-            rows[i].appendChild(singleSquere);
-            rows[i].appendChild(spacing);
+            row.appendChild(singleSquere);
+            row.appendChild(spacing);
+            console.log(rows[0].childNodes[3]);
 
-
-            //Кастыльный блок
-            // для коректной работы нужно указывать в continue метку outer на внешнем цикле
-            /*for (let x = 0; x < innerRow.childNodes.length; x++) {
-                let item = innerRow.childNodes[x];
-                if (item.nodeName === "DIV") {
-                    let singleSquere = document.querySelector('.squere-center').cloneNode(true);
-                    item.parentNode.appendChild(singleSquere);
-                    continue outer;
-                }
-            }*/
         }
+
+
     });
     //removeRow
     minusLeft.addEventListener('click', (e) => {
-       console.log("Удалить строку");
+
+        if (rows.length === 1) {
+            return false;
+        }
+
+        center.removeChild(rows[rows.length - 1]);
+
+        if (rows.length === 1) {
+            minusLeft.style.visibility = 'hidden';
+        }
+
     });
     //removeColumn
     minusTop.addEventListener('click', (e) => {
-        console.log("Удалить столбец");
+        outer :for (let i = 0; i < rows.length; i++) {
+            let row = rows[i];
+            if (row.childNodes.length <= 3) {
+                return false;
+            }
+            for (let x = 0; x < row.childNodes.length; x++) {
+                let item = row.childNodes[x];
+                item.parentNode.removeChild(row.childNodes[row.childNodes.length - 1]);
+                item.parentNode.removeChild(row.childNodes[row.childNodes.length - 1]);
+                if (row.childNodes.length <= 3) {
+                    minusTop.style.visibility = 'hidden';
+                }
+                continue outer;
+            }
+        }
+        console.log(rows[0].childNodes[3]);
     });
 
 };
 
+
+//Кастыльный блок
+// для коректной работы нужно указывать в continue метку outer на внешнем цикле
+/*for (let x = 0; x < innerRow.childNodes.length; x++) {
+    let item = innerRow.childNodes[x];
+    if (item.nodeName === "DIV") {
+        let singleSquere = document.querySelector('.squere-center').cloneNode(true);
+        item.parentNode.appendChild(singleSquere);
+        continue outer;
+    }
+}*/
