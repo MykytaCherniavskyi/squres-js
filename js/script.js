d732bdefaa508626c38ecf60dbccb51147ea9abe
mikitty_ch <mikita1234qwer@gmail.com>
2018-10-06 23:12:39 +0300

    Перенес старый код в класс и улучшил его работу.

--- js/script.js
+++ js/script.js
@@ -1,216 +1,193 @@
 window.onload = (e) => {
 
-    //controls
-    let minusTop = document.querySelector('.up').firstElementChild;
-    let minusLeft = document.querySelector('.left').firstElementChild;
-    let plusRight = document.querySelector('.right').firstElementChild;
-    let plusBottom = document.querySelector('.bottom').firstElementChild;
+    class Table {
 
-    //environment
-    let center = document.querySelector('.center');
-    let offsets;
-    let invisibleTop = false;
-    let invisibleLeft = false;
+        constructor (idElement){
 
-    // squeres array
-    let rows = document.getElementsByClassName('row');
+            this.table = document.getElementById(idElement);
 
-    //addRows
-    plusBottom.addEventListener('click', (e) => {
-        let row = rows[0].cloneNode(true);
-        center.appendChild(row);
-        if (rows.length > 1) {
-            minusLeft.style.visibility = 'visible';
-            invisibleLeft = false;
-        }
-    });
-    //addColumn
-    plusRight.addEventListener('click', (e) => {
-        for (let i = 0; i < rows.length; i++) {
-            let row = rows[i];
-            let singleSquere = document.querySelector('.row .squere').cloneNode(true);
-            row.appendChild(singleSquere);
-            if (rows[0].children.length > 1) {
-                minusTop.style.visibility = 'visible';
-                invisibleTop = false;
-            }
-        }
+            this.minusTop = this.table.getElementsByClassName('up')[0].firstElementChild;
+            this.minusLeft = this.table.getElementsByClassName('left')[0].firstElementChild;
+            this.plusRight = this.table.getElementsByClassName('right')[0].firstElementChild;
+            this.plusBottom = this.table.getElementsByClassName('bottom')[0].firstElementChild;
 
-    });
-
-    let nextRow = center.firstElementChild;
-    //removeRow
-    minusLeft.addEventListener('click', (e) => {
-        //Проверка, чтобы не удалялась последняя строка
-        if (rows.length === 1) {
-            return false;
-        }
-
-        //Удаление
-        if (currentRow != null) {
-            nextRow = currentRow.nextElementSibling;
-        }
-
-        if (currentRow != null) {
-            center.removeChild(currentRow);
-        }
-
-        // Удаление последней строки и смещение контрола
-        if (nextRow == null) {
-
-            offsets.offsetLeft = e.target.getBoundingClientRect().top - center.getBoundingClientRect().top;
-            minusLeft.style.top = offsets.offsetLeft - 52 + 'px';
-
-            nextRow = rows[rows.length - 1];
-        }
-
-        currentRow = nextRow;
-
-
-
-        currentInnerDiv = center.children[0].children[0];
-
-        //Скрытие контрола при последней строке
-        if (rows.length === 1) {
-            minusLeft.style.visibility = 'hidden';
-            invisibleLeft = true;
-        }
-
-    });
-
-
-    let currentInnerDiv = center.children[0].children[0];
-    let itemPlace = 0;
-    //removeColumn
-    minusTop.addEventListener('click', (e) => {
-
-
-
-        //Нахождение текущего элемента в коллекции
-        if (currentInnerDiv != null) {
-           outer: for (let i = 0; i < rows.length; i++) {
-               let row = rows[i];
-
-               itemPlace = 0;
-                for (let x = 0; x < row.children.length; x++) {
-                    let item = row.children[x];
-                    if (item !== currentInnerDiv) {
-                        itemPlace++;
-                    } else  {
-                        break outer;
-                    }
-                }
-
-            }
-        }
-
-        for (let i = 0; i < rows.length; i++) {
-            let row = rows[i];
-
-            if (row.children.length <= 1) {
-                minusTop.style.visibility = 'hidden';
-                return false;
-            }
-
-            let item = row.children[itemPlace];
-
-            if (item == null) item = row.children[row.children.length - 1];
-
-
-            if (item.nextElementSibling == null) {
-                offsets.offsetTop = e.target.getBoundingClientRect().left - center.getBoundingClientRect().left;
-                minusTop.style.left = offsets.offsetTop - 52 + 'px';
-            }
-
-            item.remove();
-
-            if (row.children.length <= 1) {
-                minusTop.style.visibility = 'hidden';
-                invisibleTop = true;
-            }
-
-        }
-
-        currentInnerDiv = null;
-    });
-
-    let currentRow = center.firstElementChild;
-
-    //visible or hidden minusControl buttons
-    center.addEventListener('mouseover', (e) => {
-
-        checkVisible();
-
-
-        offsets =
-            {
-                offsetLeft: e.target.getBoundingClientRect().top - center.getBoundingClientRect().top,
-                offsetTop: e.target.getBoundingClientRect().left - center.getBoundingClientRect().left
+            //environment
+            this.center = this.table.querySelector('.center');
+            this.offsets = {
+                offsetLeft: 0,
+                offsetTop: 0
+            };
+            this.positions = {
+                div: 0,
+                row: 0
             };
 
 
-        //Фильтрация чисто элементов центральной таблицы (контролы удалены)
-        let allSquares = document.getElementsByClassName('squere');
-        let inCenterSquares = Array.prototype.filter.call(allSquares, function(allSquares){
-            return allSquares.className === 'squere';
-        });
+            // squeres array
+            this.rows = this.table.getElementsByClassName('row');
 
-        // Проверяю, что фокус именно на эллементе таблицы
-        for (let i = 0; i < inCenterSquares.length; i++) {
+            this.createElement = (type, outerItem, innerItem) => {
 
-            if  (inCenterSquares[i] === e.target) {
+                const div = document.createElement('div');
 
-                currentRow = e.target.parentNode;
-                currentInnerDiv = e.target;
+                switch (type) {
 
-                minusTop.style.left = offsets.offsetTop + 'px';
-                minusLeft.style.top = offsets.offsetLeft + 'px';
+                    case 'div':
+                        div.classList.add(...outerItem);
+                        return div;
+                    case 'row':
+                        //cloneNode = false для обратной совместимости firefox 13 и т.д.
+                        const row = div.cloneNode(false);
+                        row.classList.add(...outerItem);
 
-            }
+                        const count = this.center.firstElementChild.childElementCount;
+
+                        for (let i = 0; i < count; i++) row.append(div.cloneNode(false));
+
+                        for (let div of row.children) div.classList.add(...innerItem);
+
+
+                        return row;
+
+                }
+            };
+
+
+            //addRows
+            this.plusBottom.addEventListener('click', (e) => {
+                //let row = rows[0].cloneNode(true);
+                let row = this.createElement('row',['row'],['squere']);
+                this.center.appendChild(row);
+                const count = this.rows.length;
+                if (count > 1) {
+                    this.minusLeft.style.visibility = 'visible';
+                }
+            });
+            //addColumn
+            this.plusRight.addEventListener('click', (e) => {
+                const countRows = this.rows.length;
+                for (let i = 0; i < countRows; i++) {
+                    let row = this.rows[i];
+                    let div = this.createElement('div',['squere']);
+                    row.appendChild(div);
+                    const countRowChildren = this.rows[0].children.length;
+                    if (countRowChildren > 1) {
+                        this.minusTop.style.visibility = 'visible';
+                    }
+                }
+
+            });
+
+            //removeRow
+            this.minusLeft.addEventListener('click', (e) => {
+                //Проверка, чтобы не удалялась последняя строка
+                if (this.rows.length === 1) return false;
+
+                let row = this.rows[this.positions.row];
+
+                if (row == null) row = this.rows[this.rows.length - 1];
+
+                if (row.nextElementSibling == null) {
+                    this.offsets.offsetLeft = e.target.getBoundingClientRect().top - this.center.getBoundingClientRect().top;
+                    this.minusLeft.style.top = this.offsets.offsetLeft - 55 + 'px';
+                }
+
+
+                row.remove();
+
+                //Скрытие контрола при последней строке
+                if (this.rows.length === 1) this.minusLeft.style.visibility = 'hidden';
+
+            });
+
+            //removeColumn
+            this.minusTop.addEventListener('click', (e) => {
+
+                for (let row of this.rows) {
+
+                    if (row.children.length === 1) return false;
+
+                    let div = row.children[this.positions.div];
+
+                    if (div == null) div = row.children[row.children.length - 1];
+
+                    if (div.nextElementSibling == null) {
+                        this.offsets.offsetTop = e.target.getBoundingClientRect().left - this.center.getBoundingClientRect().left;
+                        this.minusTop.style.left = this.offsets.offsetTop - 54.5 + 'px';
+                    }
+
+                    div.remove();
+
+                    if (row.children.length <= 1) this.minusTop.style.visibility = 'hidden';
+
+                }
+
+            });
+
+            this.table.addEventListener('mouseout', (e) => {
+
+                const target = e.target;
+
+                if (target === this.center
+                    || target === target.parentElement
+                    || target.classList.contains('squere')) {
+
+                    this.minusLeft.style.visibility = 'hidden';
+                    this.minusTop.style.visibility = 'hidden';
+
+                }
+
+            });
+
+            this.table.addEventListener('mouseover', (e) => {
+
+                const target = e.target;
+
+                if (target === this.center
+                    || target === target.parentElement
+                    || target.classList.contains('squere')) {
+
+                    const rowsL = this.center.childElementCount;
+                    const divsL = this.center.firstElementChild.childElementCount;
+
+                    if  (rowsL > 1) this.minusLeft.style.visibility = 'visible';
+                    else this.minusLeft.style.visibility = 'hidden';
+                    if  (divsL > 1) this.minusTop.style.visibility = 'visible';
+                    else this.minusTop.style.visibility = 'hidden';
+
+                    this.offsets.offsetLeft = e .target.getBoundingClientRect().top - this.center.getBoundingClientRect().top - 3;
+                    this.offsets.offsetTop = e.target.getBoundingClientRect().left - this.center.getBoundingClientRect().left - 3;
+
+                    //Фильтрация чисто элементов центральной таблицы (контролы удалены)
+                    let allSquares = this.table.getElementsByClassName('squere');
+                    let inCenterSquares = Array.prototype.filter.call(allSquares, function(allSquares){
+                        return allSquares.className === 'squere';
+                    });
+
+                    // Проверяю, что фокус именно на эллементе таблицы
+                    if  (inCenterSquares.includes(e.target)) {
+
+                        const currentRow = e.target.parentElement;
+                        const currentInnerDiv = e.target;
+
+                        this.positions.row = Array.from(this.center.children).indexOf(currentRow);
+                        this.positions.div = Array.from(currentRow.children).indexOf(currentInnerDiv);
+
+                        this.minusTop.style.left = this.offsets.offsetTop + 'px';
+                        this.minusLeft.style.top = this.offsets.offsetLeft + 'px';
+
+                    }
+
+                }
+
+            });
 
         }
 
-    });
-    center.addEventListener('mouseout', (e) => {
 
-        minusTop.style.visibility = 'hidden';
-        minusLeft.style.visibility = 'hidden';
-
-    });
-
-    center.addEventListener('mouseout', (e) => {
-        minusTop.style.visibility = 'hidden';
-        minusLeft.style.visibility = 'hidden';
-    });
-
-    minusLeft.addEventListener('mouseover', (e) => {
-        checkVisible();
-    });
-    minusLeft.addEventListener('mouseout', (e) => {
-        minusTop.style.visibility = 'hidden';
-        minusLeft.style.visibility = 'hidden';
-    });
-
-    minusTop.addEventListener('mouseover', (e) => {
-        checkVisible();
-    });
-    minusTop.addEventListener('mouseout', (e) => {
-        minusTop.style.visibility = 'hidden';
-        minusLeft.style.visibility = 'hidden';
-    });
-
-    function checkVisible() {
-        if (invisibleTop) {
-            minusTop.style.visibility = 'hidden';
-        } else {
-            minusTop.style.visibility = 'visible';
-        }
-
-        if (invisibleLeft) {
-            minusLeft.style.visibility = 'hidden';
-        } else {
-            minusLeft.style.visibility = 'visible';
-        }
     }
+
+
+    const t1 = new Table('first');
 
 };
